46772d70c2536495976186459c75c1fb6768aeb1
Antonio Cancemi <antoniocancemi1@gmail.com>
2023-07-05 09:49:57 +0200

    .

--- api/src/main/java/com/vigimod/api/ApiApplication.java
+++ api/src/main/java/com/vigimod/api/ApiApplication.java
@@ -1,14 +1,7 @@
 package com.vigimod.api;
 
-import java.time.LocalDateTime;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.vigimod.api.Report.ReportFileUtils;
-import com.vigimod.api.entity.Ad;
-import com.vigimod.api.entity.Report;
-import com.vigimod.api.utils.ReportCategory;
 
 @SpringBootApplication
 public class ApiApplication {
